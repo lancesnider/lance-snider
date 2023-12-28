035c2b22b5e0c4fb77335462ad8b1f0d4e09a231
Lance <lance@lancesnider.com>
2023-12-28 14:29:23 -0800

    Add masonry layout

---
+++ src/utils/storybook.ts
@@ -0,0 +1,99 @@
+import rhinoArmy from '../assets/gallery-sm/rhinoArmy.jpg'
+import verticAlley from '../assets/gallery-sm/verticAlley.jpg'
+import blindWolf from '../assets/gallery-sm/blindWolf.jpg'
+import kraken from '../assets/gallery-sm/kraken.jpg'
+import deadBird from '../assets/gallery-sm/dead-bird.jpg'
+import hermit from '../assets/gallery-sm/hermit.jpg'
+import birdsReal from '../assets/gallery-sm/birds-are-real.jpg'
+import hogwarts from '../assets/gallery-sm/hogwarts.jpg'
+import humpback from '../assets/gallery-sm/humpback.jpg'
+import sasquatch from '../assets/gallery-sm/sasquatch.jpg'
+import glassOrca from '../assets/gallery-sm/glass-orca.jpg'
+// import capitolRiot from '../assets/gallery-sm/capitol-riot-preview.jpg'
+import capitolRiot from '../assets/gallery-sm/jan6-2.jpg'
+import octopusCave from '../assets/gallery-sm/octopus-cave.jpg'
+import octopusCharcoal from '../assets/gallery-sm/octopus-large.jpg'
+import octopus from '../assets/gallery-sm/octopus.jpg'
+import witch from '../assets/gallery-sm/witch.jpg'
+import skeleton from '../assets/gallery-sm/skeleton.jpg'
+import crocTeeth from '../assets/gallery-sm/croc-teeth.jpg'
+import glinesCanyonDam from '../assets/gallery-sm/glines-canyon-dam.jpg'
+import heart from '../assets/gallery-sm/heart.jpg'
+import littleFactories from '../assets/gallery-sm/little-factories.jpg'
+// import hogwartsBw from '../assets/gallery-sm/hogwarts-bw.jpg'
+import hogwartsBw from '../assets/gallery-sm/hogwarts-wip.jpg'
+import cassowary from '../assets/gallery-sm/cassowary.jpg'
+import zombies from '../assets/gallery-sm/zombies.jpg'
+
+const images = [
+  { image: rhinoArmy, alt: 'Rhino Army' },
+  { image: verticAlley, alt: 'Vertic Alley' },
+  { image: birdsReal, alt: '' },
+  { image: glassOrca, alt: '' },
+  { image: sasquatch, alt: '' },
+  { image: blindWolf, alt: 'Blind Wolf' },
+  { image: witch, alt: '' },
+  { image: kraken, alt: 'Kraken' },
+  { image: hogwarts, alt: '' },
+  { image: deadBird, alt: '' },
+  { image: hermit, alt: '' },
+  { image: glinesCanyonDam, alt: '' },
+  { image: humpback, alt: '' },
+  { image: hogwartsBw, alt: '' },
+  { image: skeleton, alt: '' },
+  { image: heart, alt: '' },
+  { image: crocTeeth, alt: '' },
+  { image: octopusCharcoal, alt: '' },
+  { image: zombies, alt: '' },
+  { image: octopus, alt: '' },
+  { image: cassowary, alt: '' },
+  { image: octopusCave, alt: '' },
+  { image: capitolRiot, alt: 'Jan 6' },
+  { image: littleFactories, alt: '' },
+]
+
+const imagesGrouped = [
+  { image: rhinoArmy, alt: 'Rhino Army' },
+  { image: verticAlley, alt: 'Vertic Alley' },
+  {
+    imageGroup: [
+      { image: glassOrca, alt: '' },
+      { image: cassowary, alt: '' },
+    ],
+  },
+  { image: hogwartsBw, alt: '' },
+  { image: birdsReal, alt: '' },
+  { image: blindWolf, alt: 'Blind Wolf' },
+  { image: kraken, alt: 'Kraken' },
+  { image: witch, alt: '' },
+  { image: sasquatch, alt: '' },
+  {
+    imageGroup: [
+      { image: glinesCanyonDam, alt: '' },
+      { image: deadBird, alt: '' },
+    ],
+  },
+  { image: octopusCharcoal, alt: '' },
+  { image: hogwarts, alt: '' },
+  { image: hermit, alt: '' },
+  { image: humpback, alt: '' },
+  {
+    imageGroup: [
+      { image: skeleton, alt: '' },
+      { image: octopus, alt: '' },
+    ],
+  },
+  { image: crocTeeth, alt: '' },
+  { image: zombies, alt: '' },
+  { image: octopusCave, alt: '' },
+
+  {
+    imageGroup: [
+      { image: heart, alt: '' },
+      { image: littleFactories, alt: '' },
+    ],
+  },
+  { image: capitolRiot, alt: 'Jan 6' },
+]
+
+export { images, imagesGrouped }
