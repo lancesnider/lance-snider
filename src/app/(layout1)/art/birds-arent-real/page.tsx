cfa3b6de7c4e488048ddca262fa5ac4b3f94676d
Lance <lance@lancesnider.com>
2024-02-16 09:47:48 -0800

    So many changes I should hae done in 20 commits. Oops

--- src/app/(layout1)/art/birds-arent-real/page.tsx
+++ src/app/(layout1)/art/birds-arent-real/page.tsx
@@ -1,11 +1,13 @@
 import Image from '@/components/Image/Image'
 import Details from '@/components/Details/Details'
+import Seagulls from '@/components/Seagulls/Seagulls'
 
 import image from '@/assets/gallery-sm/birds-are-real.jpg'
 
 export default function Page() {
   return (
     <div>
+      <Seagulls />
       <Image image={image} alt='Just Seagull Stuff' />
       <Details
         subheading='Pen & watercolor then digital (Procreate)'
--- src/app/(layout1)/layout.tsx
+++ src/app/(layout1)/layout.tsx
@@ -1,21 +1,23 @@
 import type { Metadata } from 'next'
 
-import { Inter } from 'next/font/google'
+import Header from '@/components/Header/Header'
+import Footer from '@/components/Footer/Footer'
 
-import './globals.scss'
 import '@/app/page.scss'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Lance Snider - Artist & Dev',
   description: 'I make pretty things.',
 }
 
+// const devLink = 'https://main--658df898ec129ee3d5a882ae.chromatic.com'
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en' className='page'>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <div className='layout'>
+      <Header />
+      {children}
+      <Footer />
+    </div>
   )
 }
