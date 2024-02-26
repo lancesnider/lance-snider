95a24e3f862757d30b0bc7b008066a196b1d2bd2
Lance <lance@lancesnider.com>
2024-02-26 14:09:04 -0800

    Cassowary animation

---
+++ src/app/(layout1)/dev/case-studies/cassowary/page.tsx
@@ -0,0 +1,12 @@
+import BlogContent from '@/components/BlogContent/BlogContent'
+
+import Cassowary from '@/components/Cassowary/Cassowary'
+
+export default function Page() {
+  return (
+    <BlogContent>
+      <h1>Interactive Web Animation with Rive & React</h1>
+      <Cassowary />
+    </BlogContent>
+  )
+}
