7ba05bc14d168f2d03df930bfcbc968a4c5e75a0
Lance <lance@lancesnider.com>
2024-03-23 11:34:04 -0700

    Seagulls case study page

---
+++ src/app/(layout1)/dev/case-studies/seagulls/page.tsx
@@ -0,0 +1,12 @@
+import BlogContent from '@/components/BlogContent/BlogContent'
+
+import Seagulls from '@/components/Seagulls/Seagulls'
+
+export default function Page() {
+  return (
+    <BlogContent>
+      <h1>Interactive Web Animation with Rive & React</h1>
+      <Seagulls />
+    </BlogContent>
+  )
+}
