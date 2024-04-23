4150ff2ae9d477ff176e56ccbdb601c7842fcded
Lance <lance@lancesnider.com>
2024-04-22 18:45:02 -0700

    Handle duck positions

--- src/components/DuckHunt/DuckHunt.tsx
+++ src/components/DuckHunt/DuckHunt.tsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react'
 import { times } from 'lodash'
+import gsap from 'gsap'
 import RiveCanvas, {
   Artboard,
   Renderer,
@@ -22,26 +23,81 @@ const CANVAS_HEIGHT = 720
 
 import './DuckHunt.scss'
 
+/*
+  Game Logic
+
+  - Home Screen
+    - Show top score (saved to localstorage)
+    - Click 1 duck or 2 ducks, which takes you to Game Screen
+      - Instantiate ducks (The same duck instances get reused through the game)
+  - Round Start
+    - Reset round variables
+    - When round animation (dog sniffing) completes, start turn
+  - Turn
+    - Random path is created for ducks to follow
+    - Gun becomes active
+    - Clicking anywhere fires shot, reducing ammo
+      - When ammo is 0, ducks fly away, end turn
+    - Clicking duck kills it
+      - When all ducks are dead, end turn
+  - End Turn
+    - Reset turn variables (ammo, duck positions, etc.)
+    - turnNumber++
+    - Display end turn animation (dog laughing or holding duck(s))
+    - If ducks released in this round == 10, end round
+    - If ducks released in this round < 10, start next turn
+  - End Round
+    - Check if game over
+    - End round animation
+    - roundNumber++
+    - Reset round variables
+    - Update high score (if applicable)
+  - Game Over
+    - Game over animation
+    - Reset everything
+    - Go to Home Screen
+*/
+
 enum GameState {
-  HOME = 0,
-  NEW_ROUND = 1,
+  HOME_SCREEN = 0,
+  ROUND_START = 1,
   TURN = 2,
-  BETWEEN_TURNS = 3,
+  END_TURN = 3,
+  END_ROUND = 4,
+  GAME_OVER = 5,
 }
+
+const DUCK_WIDTH = 108
+const DUCK_HEIGHT = 105
+const ROUND_DURATION = 5
 
 const DuckHunt = () => {
   const canvasRef = useRef(null)
 
   useEffect(() => {
+    let currentRound = 0
+    let currentTurn = 0
+    let duckCount = 1
+    let ducksKilledPerRound = 0
+    let ducksLandeddPerRound = 0
+    let ducksKilledPerTurn = 0
+    let missesPerTurn = 0
+    let currentAmmo = 3
+    let currentScore = 0
+    let currentGameState = GameState.HOME_SCREEN
+
     let renderer: Renderer | null = null
     let file: File | null = null
-    const ducks: {
+    let ducks: {
       duckArtboard?: Artboard
       duckStateMachine: StateMachineInstance | null
       isGlideInput?: SMIInput | null
       isRightInput?: SMIInput | null
       resetTrigger?: SMIInput | null
       dieTrigger?: SMIInput | null
+      timeline?: gsap.core.Timeline | null
+      position: { x: number; y: number }
+      previousPosition: { x: number; y: number }
     }[] = []
     const redDucks: {
       redDuckArtboard?: Artboard
@@ -54,17 +110,20 @@ const DuckHunt = () => {
     let handleMove: (e: MouseEvent) => void | null
     const mainCanvas = canvasRef.current as HTMLCanvasElement | null
 
-    let currentRound = 0
-    let duckCount = 1
+    const removeDucks = () => {
+      ducks.forEach(({ duckStateMachine, duckArtboard }) => {
+        duckArtboard?.delete()
+        duckStateMachine?.delete()
+      })
+      ducks = []
+    }
 
     const loadDuckHunt = async () => {
-      if (!canvasRef.current) return
+      if (!canvasRef.current || !mainCanvas) return
 
       const rive = await RiveCanvas({
         locateFile: (_) => RIVE_WASM_URL,
       })
-
-      if (!mainCanvas) return
 
       mainCanvas.width = CANVAS_WIDTH
       mainCanvas.height = CANVAS_HEIGHT
@@ -84,7 +143,32 @@ const DuckHunt = () => {
         mainArtboard
       )
 
-      // Load 8 instances of the red ducks
+      /*
+        Pass mouse move/click events to the Rive State Machine
+      */
+      handleClick = (e: MouseEvent) => {
+        const rect = mainCanvas.getBoundingClientRect()
+        const mouseX = e.clientX - rect.left
+        const mouseY = e.clientY - rect.top
+
+        mainStateMachine?.pointerDown(mouseX, mouseY)
+      }
+      mainCanvas.addEventListener('click', handleClick)
+      handleMove = (e: MouseEvent) => {
+        const rect = mainCanvas.getBoundingClientRect()
+        const mouseX = e.clientX - rect.left
+        const mouseY = e.clientY - rect.top
+
+        mainStateMachine?.pointerMove(mouseX, mouseY)
+      }
+      mainCanvas.addEventListener('mousemove', handleMove)
+
+      /*
+        Instantiate nested artboards
+      */
+
+      // Add 10 red ducks to the UI
+      // These are used to indicate active turn and ducks hit/missed
       times(10, () => {
         const redDuckArtboard = file?.artboardByName('redDuck')
         const redDuckStateMachine = getStateMachineByName(
@@ -102,7 +186,9 @@ const DuckHunt = () => {
         })
       })
 
-      // create ducks (this happens when clicking play on the home screen)
+      // Instantiate ducks (this happens when clicking play on the home screen)
+      // The same duck(s) get reused throughout turns and rounds
+      // Remove on Game Over
       const instantiateDucks = (ducksToInstantiate: number) => {
         duckCount = ducksToInstantiate
 
@@ -126,77 +212,186 @@ const DuckHunt = () => {
             isRightInput,
             resetTrigger,
             dieTrigger,
+            position: { x: 0, y: 0 },
+            previousPosition: { x: 0, y: 0 },
           })
         })
       }
 
       /*
-        Inputs
+        Game Inputs
       */
       const gameStateInput = getInput('gameState', mainStateMachine)
       const ammoInput = getInput('ammo', mainStateMachine)
+      const turnOverTrigger = getInput('turnOver', mainStateMachine)
+      const killsInput = getInput('kills', mainStateMachine)
 
-      /*
-        Text runs
-
-      */
-
+      // Text runs
       const roundNumberText = mainArtboard.textRun('roundNumber')
 
-      handleClick = (e: MouseEvent) => {
-        const rect = mainCanvas.getBoundingClientRect()
-        const mouseX = e.clientX - rect.left
-        const mouseY = e.clientY - rect.top
-
-        mainStateMachine?.pointerDown(mouseX, mouseY)
-      }
-
-      mainCanvas.addEventListener('click', handleClick)
-
-      handleMove = (e: MouseEvent) => {
-        const rect = mainCanvas.getBoundingClientRect()
-        const mouseX = e.clientX - rect.left
-        const mouseY = e.clientY - rect.top
-
-        mainStateMachine?.pointerMove(mouseX, mouseY)
-      }
-
-      mainCanvas.addEventListener('mousemove', handleMove)
-
       /*
-        Rive events
+        -----------------------------------------
+        -------------- GAME LOGIC --------------
+        -----------------------------------------
       */
 
-      const releaseDucks = () => {
-        // console
-        resetTurn()
+      const startGame = (duckCount: number) => {
+        instantiateDucks(duckCount)
+        currentAmmo = 3
+        beginRound()
       }
 
-      const resetTurn = () => {
-        currentRound++
-        if (ammoInput) ammoInput.value = 3
+      const beginRound = () => {
+        // reset red ducks in ui
+        ducksKilledPerRound = 0
       }
 
-      const handleGameStateEvent = ({ properties }: any) => {
-        if (!properties) return
+      const beginTurn = () => {
+        currentAmmo = 3
+        missesPerTurn = 0
+        ducksKilledPerTurn = 0
+        // activate gun
+        // release ducks
+        ducks.map(({ position, isRightInput }, index) => {
+          // duckStateMachine?.fireTrigger('reset')
+          const duckTl = gsap.timeline()
 
-        const { currentGameState } = properties
+          let xPos = 205 + 205 * index
+          let yPos = 465
 
-        switch (currentGameState) {
-          case 'roundStart':
-            currentRound++
-            roundNumberText.text = currentRound.toString()
-            return
-          case 'releaseDucks':
-            releaseDucks()
-            return
-          default:
-            return
+          duckTl.set(position, {
+            x: xPos,
+            y: yPos,
+          })
+
+          let totalDuration = 0
+
+          while (totalDuration < ROUND_DURATION) {
+            const newXPos =
+              Math.floor(Math.random() * CANVAS_WIDTH) - DUCK_WIDTH / 2
+            const newYPos = Math.floor(Math.random() * 400) - DUCK_HEIGHT / 2
+
+            const distance = Math.sqrt(
+              Math.pow(newXPos - xPos, 2) + Math.pow(newYPos - yPos, 2)
+            )
+
+            const duration = distance / 300
+            totalDuration += duration
+
+            duckTl.to(position, {
+              x: newXPos,
+              y: newYPos,
+              duration: duration,
+              ease: 'linear',
+            })
+
+            xPos = newXPos
+            yPos = newYPos
+          }
+
+          // fly offscreen
+          const finalXPos =
+            Math.floor(Math.random() * CANVAS_WIDTH) - DUCK_WIDTH / 2
+          const finalYPos = -110
+          const distance = Math.sqrt(
+            Math.pow(finalXPos - xPos, 2) + Math.pow(finalYPos - yPos, 2)
+          )
+          const finalDuration = distance / 300
+          duckTl.to(position, {
+            x: finalXPos,
+            y: finalYPos,
+            duration: finalDuration,
+            ease: 'linear',
+            onComplete: () => {
+              missesPerTurn++
+
+              if (missesPerTurn === duckCount) {
+                endTurn()
+              }
+            },
+          })
+        })
+      }
+
+      const duckMissed = () => {
+        currentAmmo -= 1
+        missesPerTurn++
+
+        if (currentAmmo === 0) {
+          // trigger fly away
+          // set round over in Rive
         }
       }
 
+      const duckHit = () => {
+        console.log('duck hit')
+        currentAmmo -= 1
+        ducksKilledPerTurn++
+
+        // update score (missesPerTurn 0 = 1000, 1 = 500, 2 = 250)
+        // Update red ducks in ui
+      }
+
+      const duckLanded = () => {
+        ducksLandeddPerRound++
+
+        if (ducksKilledPerTurn === duckCount) {
+          endTurn()
+        }
+      }
+
+      const endTurn = () => {
+        if (killsInput && turnOverTrigger) {
+          killsInput.value = ducksKilledPerTurn
+          turnOverTrigger?.fire()
+        }
+      }
+
+      const endTurnAnimationEnd = () => {
+        currentTurn++
+        if (currentTurn * duckCount === 10) {
+          endRound()
+        } else {
+          beginTurn()
+        }
+      }
+
+      const endRound = () => {
+        // update high score
+        // shift red ducks in ui until red are all to the left, white to the right
+        if (ducksKilledPerRound >= 6) {
+          currentRound++
+          beginRound()
+        } else {
+          gameOver()
+        }
+      }
+
+      const gameOver = () => {
+        // show game over animation for 5 seconds
+        // reset game
+        resetGame()
+      }
+
+      const resetGame = () => {
+        currentRound = 0
+        currentTurn = 0
+        duckCount = 1
+        ducksKilledPerRound = 0
+        ducksKilledPerTurn = 0
+        missesPerTurn = 0
+        currentAmmo = 3
+        currentScore = 0
+
+        if (gameStateInput) gameStateInput.value = 0
+
+        removeDucks()
+      }
+
       /*
-        Render Loop
+        -----------------------------------------
+        -------------- Render Loop --------------
+        -----------------------------------------
       */
 
       let lastTime = 0
@@ -210,24 +405,33 @@ const DuckHunt = () => {
         const elapsedTimeSec = elapsedTimeMs / 1000
         lastTime = time
 
-        // Rive events
+        /*
+          Rive Event Handling
+          Listen for events fired by Rive
+        */
         const numFiredEvents = mainStateMachine.reportedEventCount()
         for (let i = 0; i < numFiredEvents; i++) {
           const event = mainStateMachine.reportedEventAt(i)
           // Run any Event-based logic now
           if (!event) return
 
+          console.log('Rive Event:', event.name)
+
           switch (event.name) {
-            case 'gameStateEvent':
-              handleGameStateEvent(event)
-              break
             case 'startGame1Duck':
-              console.log('startGame1Duck')
-              instantiateDucks(1)
+              startGame(1)
               break
             case 'startGame2Ducks':
-              console.log('startGame2Ducks')
-              instantiateDucks(2)
+              startGame(2)
+              break
+            case 'beginRoundAnimationEnd':
+              beginTurn()
+              break
+            case 'turnOverAnimationEnd':
+              endTurnAnimationEnd()
+              break
+            case 'duckKilled':
+              duckHit()
               break
             default:
               break
@@ -245,7 +449,7 @@ const DuckHunt = () => {
         renderer.restore()
 
         // get mouse x position relative to the canvas
-        if (gameStateInput?.value !== GameState.HOME) {
+        if (gameStateInput?.value !== GameState.HOME_SCREEN) {
           redDucks.map(({ redDuckArtboard, redDuckStateMachine }, index) => {
             if (renderer && redDuckArtboard) {
               redDuckStateMachine?.advance(elapsedTimeSec)
@@ -272,29 +476,38 @@ const DuckHunt = () => {
         }
 
         // Render ducks
-        ducks.map(({ duckArtboard, duckStateMachine }, index) => {
-          if (renderer && duckArtboard) {
-            duckStateMachine?.advance(elapsedTimeSec)
-            duckArtboard?.advance(elapsedTimeSec)
-            renderer.save()
+        ducks.map(
+          (
+            {
+              duckArtboard,
+              duckStateMachine,
+              position,
+              previousPosition,
+              isRightInput,
+              isGlideInput,
+            },
+            index
+          ) => {
+            if (renderer && duckArtboard) {
+              duckStateMachine?.advance(elapsedTimeSec)
+              duckArtboard?.advance(elapsedTimeSec)
+              renderer.save()
 
-            const xPos = 256 + 256 * index
+              // set duck positions
+              if (isRightInput) {
+                isRightInput.value = previousPosition.x < position.x
+              }
+              if (isGlideInput) {
+                isGlideInput.value = previousPosition.y < position.y
+              }
+              renderer.translate(position.x, position.y)
 
-            // renderer.align(
-            //   rive.Fit.contain,
-            //   rive.Alignment.topCenter,
-            //   {
-            //     minX: xPos,
-            //     minY: 108,
-            //     maxX: xPos + 108,
-            //     maxY: 108 + 108,
-            //   },
-            //   duckArtboard.bounds
-            // )
-            duckArtboard?.draw(renderer)
-            renderer.restore()
+              duckArtboard?.draw(renderer)
+              renderer.restore()
+              ducks[index].previousPosition = { ...position }
+            }
           }
-        })
+        )
 
         rive.requestAnimationFrame(renderLoop)
       }
@@ -302,6 +515,12 @@ const DuckHunt = () => {
     }
 
     loadDuckHunt()
+
+    /*
+        -----------------------------------------
+        ---------------- CLEANUP ----------------
+        -----------------------------------------
+      */
 
     return () => {
       console.log('DuckHunt unmounted')
@@ -313,10 +532,7 @@ const DuckHunt = () => {
       file?.delete()
       mainArtboard?.delete()
 
-      ducks.forEach((duck) => {
-        duck?.duckArtboard?.delete()
-        duck?.duckStateMachine?.delete()
-      })
+      removeDucks()
 
       redDucks.forEach((redDuck) => {
         redDuck?.redDuckArtboard?.delete()
