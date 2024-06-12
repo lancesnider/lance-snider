8b7ea98972d31f04b8fb4b12602b7d02d0cd2567
Lance <lance@lancesnider.com>
2024-06-12 11:40:59 -0700

    Add page for game

---
+++ src/app/(layoutNone)/duck-hunt/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import DuckHunt from '@/components/DuckHunt/DuckHunt'
+
+import './duckHunt.scss'
+
+export default function Page() {
+  return (
+    <div className='duck-hunt'>
+      <div className='duck-hunt__container'>
+        {window && window.innerWidth < 768 ? (
+          <div className='duck-hunt__desktop'>Sorry, this is desktop only</div>
+        ) : (
+          <DuckHunt />
+        )}
+      </div>
+    </div>
+  )
+}
